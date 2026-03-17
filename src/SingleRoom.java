b3490da068c28af9a1bfa82c4488dbbc1fdef9a2
rayyan506 <mrrayyan.ak007@gmail.com>
2026-03-17 16:24:28 +0530

    Use Case 3: Centralized Room Inventory Management

---
+++ src/SingleRoom.java
@@ -0,0 +1,17 @@
+/**
+ * CLASS: SingleRoom
+ *
+ * Represents a single room in the hotel.
+ *
+ * @version 2.1
+ */
+
+public class SingleRoom extends Room {
+
+    /**
+     * Initializes a SingleRoom with predefined attributes.
+     */
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
