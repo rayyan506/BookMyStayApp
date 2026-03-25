937a796c5aed0ed19eb909c9454eedd0bc264c3e
Rajesh Kumar Sabat <rajeshkumarsabat09@gmail.com>
2026-03-25 16:23:16 +0530

    Use Case 9: Error Handling & Validation

---
+++ src/InvalidBookingException.java
@@ -0,0 +1,9 @@
+public class InvalidBookingException extends Exception {
+
+    /**
+     * Constructor with message
+     */
+    public InvalidBookingException(String message) {
+        super(message);
+    }
+}
